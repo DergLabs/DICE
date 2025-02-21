47b78d4e062f8045302ac0e2fa653a067f86de40
Shahriyar Shawon <developer.shahriyar@gmail.com>
2025-02-21 12:26:37 -0500

    Statistics

--- Software/dice_reference_compression/pyqt/mainwindow.py
+++ Software/dice_reference_compression/pyqt/mainwindow.py
@@ -379,13 +379,13 @@ class MainWindow(QMainWindow):
         pixmap1 = QPixmap(oimg)
         pixmap2 = QPixmap(timg)
 
-        processed_size = target_image.shape*8
-        original_shape = f"{original_image.shape}"
-        processed_shape = f"{target_image.shape}"
+        # processed_size = target_image.shape*8
+        # original_shape = f"{original_image.shape}"
+        # processed_shape = f"{target_image.shape}"
         self.processed_window = ProcessedWindow(
             pixmap1,
             pixmap2,
-            f"PSNR: {res.PSNR:.3f} MSSSIM: {res.MSSSIM:.3f} OG Size: {res.original_size//1024//1024} Original Shape: {original_shape} Processed Shape: {processed_shape}",
+            f"PSNR: {res.PSNR:.3f} MSSSIM: {res.MSSSIM:.3f} OG Size: {res.original_size//1024//1024} Compressed Size: {res.size_stats.compressed_size//1024//1024} Compression Ratio: {res.compression_ratio}",
         )
         self.processed_window.show()
 
